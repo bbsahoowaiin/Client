b45f8566af418a476055c564e064c0cc24ec3fde
Bikram Sahoo <bikram.sahoo@waiin.com>
2023-09-06 23:48:17 +0530

    added ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client';
+  users: any;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.loadInitialData()
+  }
+  loadInitialData() {
+   this.http.get('https://localhost:5001/api/users').subscribe(res => {
+    this.users = res;
+   })
+  }
 }
